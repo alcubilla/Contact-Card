4293ef90363f1b8e25d4480a9972dacc4482675d
Marina <alcubilla.marina@gmail.com>
2020-02-12 23:13:42 -0600

    Se modifico el link de la imagen y el formato de estilo

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 const ContactCard = props => {
-  
- 
-  console.log(props.info.imgUrl);
 return(
+
+  <div className="container" id="tarjeta">
+  <div className="card border-danger mb-3" > 
+  <img src={props.info.imgUrl} alt =" No se encontro la imagen"/>
+  <div className="card-body">
+    <h3 className="card-title">{props.info.name}</h3>
+    <p className="card-text">  {props.info.phone} </p>
+    <a href="#" className="btn btn-danger">{props.info.email} </a>
+  </div>
+</div>
+</div>
+/* 
+
 <div className="card m-4">
 <img src= "{props.info.umgUrl}" alt =" No se encontro la imagen"/>
   <div className="card-body">
@@ -12,7 +22,7 @@ return(
     <p>telefono: {props.info.phone} </p>
     <p>Email: {props.info.email} </p>
   </div>
-</div>
+</div> */
 );
 }
 
