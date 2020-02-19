3c4bc9f1fb3f37d38807785d3fe077d24f8f56d5
Marina <alcubilla.marina@gmail.com>
2020-02-18 19:05:00 -0600

    se agrego la navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+const NavBar = () => {
+  return (
+ 
+<nav className="navbar bg-danger m-0 w-100 justify-content-center">
+    <h1 id="title" >Directorio</h1>
+</nav>
+    
+  );
+};
+
+export default NavBar;
